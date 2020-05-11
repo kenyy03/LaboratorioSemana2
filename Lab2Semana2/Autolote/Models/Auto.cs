e39984671ebadc6e13433c72f09401bc0a58cc09
Keny David Travanino Madrid <travanino03@gmail.com>
2020-05-10 22:23:28 -0600

    Plane1

---
+++ Lab2Semana2/Autolote/Models/Auto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Autolote.Models
+{
+    public class Auto
+    {
+        private string numPlaca;
+        private string marca;
+        private string modelo;
+        private int año;
+        private string color;
+        private string estado;
+
+        public Auto()
+        {
+            this.numPlaca = null;
+            this.marca = null;
+            this.modelo = null;
+            this.año = 0;
+            this.color = null;
+            this.estado = null;
+        }
+
+        public Auto(string pNP, string pM, string pMod, int pAño, string pColor, string pEs)
+        {
+            this.numPlaca = pNP;
+            this.marca = pM;
+            this.modelo = pMod;
+            this.año = pAño;
+            this.color = pColor;
+            this.estado = pEs;
+        }
+
+        // Setters and Getters
+        public string NumPlaca { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Año { get; set; }
+        public string Color { get; set; }
+        public string Estado { get; set; }
+
+        public void IngresarAuto()
+        {
+
+        }
+
+        public void AutosDisp()
+        {
+
+        }
+
+        public void AutosNOdisp()
+        {
+
+        }
+
+        public void VenderAuto()
+        {
+
+        }
+
+
+    }
+}
---
+++ Lab2Semana2/Autolote/Program.cs
@@ -0,0 +1,43 @@
+﻿using Autolote.Models;
+using System;
+
+namespace Autolote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto A1;
+            int option = 0;
+
+            Console.WriteLine("---Autolote---");
+            Console.Write("\nElija una opcion:\n[1] Ingresar Vehiculos\n[2] Ver todos los vehiculos disponibles" +
+                "\n[3] Ver todos los vehiculos vendidos\n[4] Vender vehiculo\n[5] Salir\n");
+            Console.Write("--->");
+            option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Entrada no valida");
+                    break;
+            }
+
+        }
+    }
+}
