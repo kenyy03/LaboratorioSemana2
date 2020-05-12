75889678a356a0d2d1645f45c3575c14fb77b6ea
Keny David Travanino Madrid <travanino03@gmail.com>
2020-05-11 23:17:15 -0600

    Class Auto

--- Lab2Semana2/Autolote/Models/Auto.cs
+++ Lab2Semana2/Autolote/Models/Auto.cs
@@ -6,61 +6,32 @@ namespace Autolote.Models
 {
     public class Auto
     {
-        private string numPlaca;
-        private string marca;
-        private string modelo;
-        private int año;
-        private string color;
-        private string estado;
+        public string NumPlaca { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+        public string Color { get; set; }
+        public string Estado { get; set; }
+
 
         public Auto()
         {
-            this.numPlaca = null;
-            this.marca = null;
-            this.modelo = null;
-            this.año = 0;
-            this.color = null;
-            this.estado = null;
+            this.NumPlaca = null;
+            this.Marca = null;
+            this.Modelo = null;
+            this.Año = 0;
+            this.Color = null;
+            this.Estado = null;
         }
 
         public Auto(string pNP, string pM, string pMod, int pAño, string pColor, string pEs)
         {
-            this.numPlaca = pNP;
-            this.marca = pM;
-            this.modelo = pMod;
-            this.año = pAño;
-            this.color = pColor;
-            this.estado = pEs;
+            this.NumPlaca = pNP;
+            this.Marca = pM;
+            this.Modelo = pMod;
+            this.Año = pAño;
+            this.Color = pColor;
+            this.Estado = pEs;
         }
-
-        // Setters and Getters
-        public string NumPlaca { get; set; }
-        public string Marca { get; set; }
-        public string Modelo { get; set; }
-        public string Año { get; set; }
-        public string Color { get; set; }
-        public string Estado { get; set; }
-
-        public void IngresarAuto()
-        {
-
-        }
-
-        public void AutosDisp()
-        {
-
-        }
-
-        public void AutosNOdisp()
-        {
-
-        }
-
-        public void VenderAuto()
-        {
-
-        }
-
-
     }
 }
--- Lab2Semana2/Autolote/Program.cs
+++ Lab2Semana2/Autolote/Program.cs
@@ -7,37 +7,144 @@ namespace Autolote
     {
         static void Main(string[] args)
         {
-            Auto A1;
-            int option = 0;
+            
+            Auto[] Autolote = new Auto[0]; ;
+            int paAño=0, option = 0,opvender=0;
+            string pNP=null, pM=null, pMod=null, pColor=null, pEs=null, opMarca=null, opModelo=null;
 
+            while (true)
+            {
+            Console.WriteLine("________________________________________________________________");
             Console.WriteLine("---Autolote---");
             Console.Write("\nElija una opcion:\n[1] Ingresar Vehiculos\n[2] Ver todos los vehiculos disponibles" +
                 "\n[3] Ver todos los vehiculos vendidos\n[4] Vender vehiculo\n[5] Salir\n");
             Console.Write("--->");
-            option = Convert.ToInt32(Console.ReadLine());
 
-            switch (option)
-            {
-                case 1:
-                    break;
+                option = Convert.ToInt32(Console.ReadLine());
 
-                case 2:
-                    break;
+                switch (option)
+                {
+                    case 1:
+                        if (Autolote.Length.Equals(0))
+                        {
+                            Autolote = new Auto[2];
+                            for (int i = 0; i < Autolote.Length; i++)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Vehiculo {0}", i+1);
+                                Console.Write("Numero de placas: ");
+                                pNP = Console.ReadLine();
+                                Console.Write("Marca: ");
+                                pM = Console.ReadLine();
+                                Console.Write("Modelo: ");
+                                pMod = Console.ReadLine();
+                                Console.Write("Año: ");
+                                paAño = Convert.ToInt32(Console.ReadLine());
+                                Console.Write("Color: ");
+                                pColor = Console.ReadLine();
+                                Console.Write("Estado: ");
+                                pEs = Console.ReadLine();
 
-                case 3:
-                    break;
+                                Autolote[i] = new Auto(pNP,pM,pMod,paAño,pColor,pEs);
+                            }
+                        }
+                        else if (Autolote.Length > 0)
+                        {
+                            Auto[] AuxAutolote = new Auto[Autolote.Length +1];
+                            for (int j = 0; j < Autolote.Length; j++)
+                            {
+                                AuxAutolote[j] = Autolote[j];
+                            }
+                            
+                            Console.WriteLine("Ingrese nuevo Auto: ");
+                            Console.Write("Numero de placas: ");
+                            pNP = Console.ReadLine();
+                            Console.Write("Marca: ");
+                            pM = Console.ReadLine();
+                            Console.Write("Modelo: ");
+                            pMod = Console.ReadLine();
+                            Console.Write("Año: ");
+                            paAño = Convert.ToInt32(Console.ReadLine());
+                            while (paAño < 1 || paAño > 2020)
+                            {
+                                Console.WriteLine("-Numero invalio- Rango entre 1 a 2020");
+                                paAño = Convert.ToInt32(Console.ReadLine());
+                            }
+                            Console.Write("Color: ");
+                            pColor = Console.ReadLine();
+                            Console.Write("Estado: ");
+                            pEs = Console.ReadLine();
 
-                case 4:
-                    break;
+                            AuxAutolote[Autolote.Length] = new Auto(pNP,pM,pMod,paAño,pColor,pEs);
 
-                case 5:
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine("Entrada no valida");
-                    break;
+                            Autolote = new Auto[AuxAutolote.Length];
+                            for (int k = 0; k < Autolote.Length; k++)
+                            {
+                                Autolote[k] = AuxAutolote[k];
+                            }
+                        }
+                            Console.ReadKey();
+                        break;
+
+                    case 2:
+                        for (int i = 0; i < Autolote.Length; i++)
+                        {
+                            if (Autolote[i].Estado == "Disponible" || Autolote[i].Estado == "disponible")
+                            {
+                                Console.WriteLine("Autos Disponibles son: {0} {1}", Autolote[i].Marca, Autolote[i].Modelo);
+                            }
+                            
+                        }
+                        Console.ReadKey();
+                        break;
+
+                    case 3:
+                        for (int i = 0; i < Autolote.Length; i++)
+                        {
+                            if (Autolote[i].Estado == "Vendido" || Autolote[i].Estado == "vendido")
+                            {
+                                Console.WriteLine("Autos Disponibles son: {0} {1}", Autolote[i].Marca, Autolote[i].Modelo);
+                            }
+
+                        }
+                        Console.ReadKey();
+                        break;
+
+                    case 4:
+                        for (int i = 0; i < Autolote.Length; i++)
+                        {
+                            if (Autolote[i].Estado == "Disponible" || Autolote[i].Estado == "disponible")
+                            {
+                                Console.WriteLine("Autos Disponibles son: {0} {1}", Autolote[i].Marca, Autolote[i].Modelo);
+                            }
+                        }
+                        Console.Write("Escriba la Marca: ");
+                        opMarca = Console.ReadLine();
+                        Console.Write("Escriba Modelo: ");
+                        opModelo = Console.ReadLine();
+
+
+                        for (int i = 0; i < Autolote.Length; i++)
+                        {
+                            if (opMarca.Equals(Autolote[i].Marca) && opModelo.Equals(Autolote[i].Modelo))
+                            {
+                                Autolote[i].Estado = "Vendido";
+                                Console.WriteLine("El auto {0} {1} ha sido {2}", Autolote[i].Marca, Autolote[i].Modelo, Autolote[i].Estado);
+                            }
+                            
+                        }
+                        Console.ReadKey();
+                        break;
+
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Entrada no valida");
+                        break;
+                        }
+                    }
+                }
             }
-
         }
-    }
-}
+    
